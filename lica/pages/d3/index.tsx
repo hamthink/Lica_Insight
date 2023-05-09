dc463a82a13d40201463de0c0aa9cd3709df76e7
이윤석 <koqj1234@gmail.com>
2023-05-09 14:30:25 +0900

    Feat:수정 - 여러 점을 기준으로 동선 그리는 로직 작성 [S08P31A208-170]

--- lica/pages/d3/index.tsx
+++ lica/pages/d3/index.tsx
@@ -16,6 +16,7 @@ import Logo from 'src/components/LogoSign';
 import Hero from 'src/content/Overview/Hero';
 import React, { useRef, useEffect, useState } from 'react';
 import * as d3 from 'd3';
+import randomColor from 'randomcolor';
 
 const HeaderWrapper = styled(Card)(
   ({ theme }) => `
@@ -55,66 +56,91 @@ const OverviewWrapper = styled(Box)(
 //   ]
 // ];
 
-const data = [
-  { x: 10, y: 10 },
-  { x: 20, y: 20 },
-  { x: 30, y: 30 },
-  { x: 40, y: 40 },
-  { x: 50, y: 50 },
-  { x: 60, y: 60 }
-];
+function get_random_color() {}
 
-const width = 600;
-const height = 400;
+function Trace({ width, height, data }) {
+  // dummy start
+  data = [
+    [
+      { x: 20, y: 20 },
+      { x: 20, y: 50 },
+      { x: 30, y: 20 },
+      { x: 40, y: 50 },
+      { x: 50, y: 20 },
+      { x: 60, y: 50 }
+    ],
+    [
+      { x: 10, y: 10 },
+      { x: 60, y: 80 },
+      { x: 70, y: 60 },
+      { x: 80, y: 80 },
+      { x: 90, y: 60 },
+      { x: 100, y: 80 }
+    ],
+    [
+      { x: 10, y: 80 },
+      { x: 30, y: 20 },
+      { x: 50, y: 50 },
+      { x: 70, y: 80 },
+      { x: 90, y: 60 },
+      { x: 100, y: 10 }
+    ]
+  ];
 
-function Trace() {
+  width = 600;
+  height = 400;
+  // dummy end
+
   const svgRef = useRef(null);
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
 
-    const xScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, (d) => d.x))
-      .range([0, width]);
-    const yScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, (d) => d.y))
-      .range([height, 0]);
+    const xScale = d3.scaleLinear().domain([0, 110]).range([0, width]);
+    const yScale = d3.scaleLinear().domain([0, 110]).range([height, 0]);
+
+    var line = d3
+      .line()
+      .x(function (d) {
+        return xScale(d.x) + 30;
+      })
+      .y(function (d) {
+        return yScale(d.y) - 30;
+      })
+      .curve(d3.curveCatmullRom.alpha(0.5)); // 곡선 형태 지정
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
     svg.attr('width', width).attr('height', height);
 
-    svg.append('g').attr('transform', 'translate(50, 370)').call(xAxis);
-    svg.append('g').attr('transform', 'translate(50, -30)').call(yAxis);
+    svg.append('g').attr('transform', 'translate(30, 370)').call(xAxis);
+    svg.append('g').attr('transform', 'translate(30, -30)').call(yAxis);
 
     svg
-      .selectAll('.dot')
+      .selectAll('circle')
       .data(data)
       .enter()
       .append('circle')
-      .attr('class', 'dot')
-      .attr('cx', (d) => xScale(d.x))
-      .attr('cy', (d) => yScale(d.y))
-      .attr('r', 5)
-      .style('fill', 'steelblue');
+      .attr('cx', function (d) {
+        return xScale(d.x) + 30;
+      })
+      .attr('cy', function (d) {
+        return yScale(d.y) - 30;
+      })
+      .attr('r', 3)
+      .attr('fill', randomColor());
 
-    svg
-      .append('path')
-      .datum(data)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 2)
-      .attr(
-        'd',
-        d3
-          .line()
-          .x((d, i) => xScale(i) + 50)
-          .y((d) => yScale(d) - 30)
-          .curve(d3.curveCatmullRom.alpha(0.5))
-      );
+    // 곡선 추가
+    data.forEach((array) => {
+      svg
+        .append('path')
+        .datum(array)
+        .attr('d', line)
+        .attr('stroke', randomColor())
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
+    });
   }, []);
 
   return (
@@ -148,7 +174,9 @@ function Trace() {
         </Container>
       </HeaderWrapper>
       <Box>
-        <svg ref={svgRef}>{/* SVG 내용 */}</svg>
+        <svg ref={svgRef} transform="translate(50, 50)">
+          {/* SVG 내용 */}
+        </svg>
       </Box>
     </OverviewWrapper>
   );
