b064de0d1d1cd9099331b44d01725efe0cb73be7
이윤석 <koqj1234@gmail.com>
2023-05-12 16:31:18 +0900

    Feat:생성 - api/visit 생성 [S08P31A208-189]

---
+++ lica/api/visit.tsx
@@ -0,0 +1,9 @@
+import { apiInstance } from 'api';
+
+const api = apiInstance();
+
+function getVisitTrack(params, success, fail) {
+  api.get(`/visit/track`, { params: params }).then(success).catch(fail);
+}
+
+export { getVisitTrack };
