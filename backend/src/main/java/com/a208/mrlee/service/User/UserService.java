e6e7ea3b0f28a69e6225026f529eb6dc87d4c326
kin0308@naver.com <kin0308@naver.com>
2023-05-12 13:33:20 +0900

    Feat: UserController fix

--- backend/src/main/java/com/a208/mrlee/service/User/UserService.java
+++ backend/src/main/java/com/a208/mrlee/service/User/UserService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public interface UserService {
     boolean idExist(String id);
+    boolean checkExist(String id);
     Map<String, Object> login(String id , String password);
     boolean isCached(String id, String code);
     void join(UserDTO userDto);
